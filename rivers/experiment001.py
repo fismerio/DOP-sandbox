e04c022d84ce1c4d8a99845a99beacc89a7091fa
fismerio <fismerio@gmail.com>
2020-08-13 22:56:42 -0300

    Improving river

--- rivers/experiment001.py
+++ rivers/experiment001.py
@@ -1,72 +1,62 @@
 import cv2
+from random import randrange
 # import numpy as np
-width = 640
-height = 480
+width = 640 * 2
+height = 480 * 2
 river = []
 shape = [width,height]
 
 def build_river(river,shape):
     # Add list of river branches.
     # Each branch adds more branch until bottom of the window.
-    # river = [[],[]]
-    x1 = shape[1] // 2
-    y1 = 0
+    green = 255
+    xMax = shape[1]
+    yMax = shape[0]
+    xMin = 0
+    yMin = 0
+    incrX = 5
+    incrY = 5
+    incrW = 1
+    incrColor = -1
+
+    x1 = xMax // 2
+    y1 = yMin
     x2 = x1
-    y2 = y1 + 50
-    color1 = (0, 255, 0)
-    thick = 20
+    y2 = y1 + incrY
+    color1 = (0, green, 0)
+    thick = 1
     river.append([(x1,y1),(x2,y2),color1,thick])
 
-    x1 = x2
-    y1 = y2
-    x2 = x1 + 50
-    y2 = y1 + 50
-    color1 = (0, 200, 0)
-    thick = 15
-    river.append([(x1,y1),(x2,y2),color1,thick])
+    while (y1 < yMax):
 
-    x1 = x2
-    y1 = y2
-    x2 = x1 - 50
-    y2 = y1 + 50
-    color1 = (0, 200, 0)
-    thick = 15
-    river.append([(x1,y1),(x2,y2),color1,thick])
+        x1 = x2
+        y1 = y2
+        # x2 = x1 + incrX
+        x2 = x1 + randrange(10) # random river
+        y2 = y1 + incrY
+        # thick = thick + incrW
+        thick = thick + randrange(2)
+        green = green + incrColor
+        color1 = (0, green, 0)
+        river.append([(x1,y1),(x2,y2),color1,thick])
 
-    x1 = x2
-    y1 = y2
-    x2 = x1 + 50
-    y2 = y1 + 50
-    color1 = (0, 150, 0)
-    thick = 10
-    river.append([(x1,y1),(x2,y2),color1,thick])
-
-    x1 = x2
-    y1 = y2
-    x2 = x1 - 50
-    y2 = y1 + 50
-    color1 = (0, 150, 0)
-    thick = 10
-    river.append([(x1,y1),(x2,y2),color1,thick])
-
+        x1 = x2
+        y1 = y2
+        # x2 = x1 - incrX
+        x2 = x1 - randrange(10) #random river
+        y2 = y1 + incrY
+        # thick = thick + incrW
+        thick = thick + randrange(2)
+        green = green + incrColor
+        color1 = (0, green, 0)
+        river.append([(x1,y1),(x2,y2),color1,thick])
 
 
 def draw_river(img, river):
     # Traverse the list river and draw each segment.
-    # for segment in river:
-    #     cv2.line(img,(segment[0][0],segment[0][1]),(segment[1][0],segment[1][1]),(0, 255, 0), 3)
 
-    # x1 = img.shape[1] // 2
-    # y1 = 0
-    # x2 = img.shape[1] // 2
-    # y2 = img.shape[0] // 2
-    # color1 = (0, 255, 0)
-    # thick = 3
     for segment in river:
         cv2.line(img, segment[0], segment[1], segment[2] , segment[3])
-
-
-
 
 # --------------------------------------------- Main --------------------------------------------------------------
 # -----------------------------------------------------------------------------------------------------------------
